917ac2f1480a4cff3a362ca88c2ce22a73982227
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-06-18 12:45:14 +0300

    Implemented adding and deleting tasks visually.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,19 +28,47 @@ export default function App() {
     });
   };
 
+  const handleAddTask = () => {
+    // get task name from input field
+    const name = (document.querySelector("input") as HTMLInputElement).value;
+    if (!name) return alert("Task name cannot be empty!");
+
+    // add task to server
+    fetch("http://localhost:1239/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: tasks.length + 1, name, completion: false }),
+    });
+  };
+
+  const handleDeleteTask = (task: Task) => {
+    // delete task from server
+    fetch(`http://localhost:1239/tasks/${task.id}`, {
+      method: "DELETE",
+    });
+  };
+
   return (
-    <ul>
-      {tasks.map((task) => (
-        // <li key={task.id}>{task.name}</li>
-        <li key={task.id}>
-          <input
-            type="checkbox"
-            checked={task.completion}
-            onChange={() => handleCheckboxChange(task)}
-          />
-          {task.name}
-        </li>
-      ))}
-    </ul>
+    <div>
+      {/* create an input field for task name */}
+      <input type="text" />
+      {/* create a button to add task */}
+      <button onClick={handleAddTask}>Add Task</button>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          // <li key={task.id}>{task.name}</li>
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.completion}
+              onChange={() => handleCheckboxChange(task)}
+            />
+            {task.id} - {task.name}
+            <button onClick={() => handleDeleteTask(task)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
