6ea6dc4eb796756fc764b37c636ab844446d7f20
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-06-18 12:27:27 +0300

    Added applying changes to the server.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 
+interface Task {
+  id: number;
+  name: string;
+  completion: boolean;
+}
+
 export default function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:1239/tasks")
@@ -9,10 +15,31 @@ export default function App() {
       .then((data) => setTasks(data));
   }, []);
 
+  const handleCheckboxChange = (task: Task) => {
+    // update task completion
+    const updatedTask = { ...task, completion: !task.completion };
+    setTasks((tasks) => tasks.map((t) => (t.id === task.id ? updatedTask : t)));
+
+    // update task completion on server
+    fetch(`http://localhost:1239/tasks/${task.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedTask),
+    });
+  };
+
   return (
     <ul>
       {tasks.map((task) => (
-        <li key={task.id}>{task.name}</li>
+        // <li key={task.id}>{task.name}</li>
+        <li key={task.id}>
+          <input
+            type="checkbox"
+            checked={task.completion}
+            onChange={() => handleCheckboxChange(task)}
+          />
+          {task.name}
+        </li>
       ))}
     </ul>
   );
